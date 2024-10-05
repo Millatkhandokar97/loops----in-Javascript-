b8b8fb8b668978e3a9fec1a4e482765f826098be
Millat Khandakar <109697680+Millatkhandokar97@users.noreply.github.com>
2024-10-05 11:20:53 +0600

    nested loops in Javascript

---
+++ nested loops.js
@@ -0,0 +1,18 @@
+let n = 10
+for(let i = 1; i <= n; i++){
+    let result = ''
+    for(let j = 1; j <= i; j++){
+        result += j + ' '
+    }
+    console.log(result);
+}
+
+
+let x = 10
+for(let i = 1; i <= x; i++){
+    let result = ''
+    for(let j = 1; j <= x; j++){
+        result += '* '
+    }
+    console.log(result);
+}
